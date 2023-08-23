53bb7eba9015e834bb8d5c512dc234b47e91033b
Ljiacheng <Ljiachenggh@gmail.com>
2023-08-23 17:15:37 +0800

    support log file num/size set

--- src/config.rs
+++ src/config.rs
@@ -6,6 +6,10 @@ pub struct RocksdbOptions {
     // TODO add more options for better usage
     pub create_if_missing: bool,
     pub atomic_flush: bool,
+    // default 5
+    pub log_file_num: usize,
+    // default 100M
+    pub log_file_size: usize,
 }
 
 impl Default for RocksdbOptions {
@@ -13,6 +17,8 @@ impl Default for RocksdbOptions {
         RocksdbOptions {
             create_if_missing: true,
             atomic_flush: true,
+            log_file_num: 5,
+            log_file_size: 100 * 1000 * 1000
         }
     }
 }
@@ -22,6 +28,8 @@ impl From<RocksdbOptions> for Options {
         let mut opt = Options::default();
         opt.create_if_missing(roc_opt.create_if_missing);
         opt.set_atomic_flush(roc_opt.atomic_flush);
+        opt.set_keep_log_file_num(roc_opt.log_file_num);
+        opt.set_max_log_file_size(roc_opt.log_file_size);
         opt
     }
 }
