40cebbf240a0eda571616cd6061a854a656806a1
kaiyu <35792093+Kayryu@users.noreply.github.com>
2023-08-24 14:54:07 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -6,10 +6,10 @@ pub struct RocksdbOptions {
     // TODO add more options for better usage
     pub create_if_missing: bool,
     pub atomic_flush: bool,
-    // default 5
-    pub log_file_num: usize,
-    // default 100M
-    pub log_file_size: usize,
+    // default 2
+    pub log_file_num: Option<usize>,
+    // default 20M
+    pub log_file_size: Option<usize>,
 }
 
 impl Default for RocksdbOptions {
@@ -17,8 +17,8 @@ impl Default for RocksdbOptions {
         RocksdbOptions {
             create_if_missing: true,
             atomic_flush: true,
-            log_file_num: 2,
-            log_file_size: 20 * 1000 * 1000
+            log_file_num: Some(2),
+            log_file_size: Some(20 * 1000 * 1000)
         }
     }
 }
@@ -28,8 +28,8 @@ impl From<RocksdbOptions> for Options {
         let mut opt = Options::default();
         opt.create_if_missing(roc_opt.create_if_missing);
         opt.set_atomic_flush(roc_opt.atomic_flush);
-        opt.set_keep_log_file_num(roc_opt.log_file_num);
-        opt.set_max_log_file_size(roc_opt.log_file_size);
+        opt.set_keep_log_file_num(roc_opt.log_file_num.unwrap_or(2));
+        opt.set_max_log_file_size(roc_opt.log_file_size.unwrap_or(20 * 1000 * 1000));
         opt
     }
 }
