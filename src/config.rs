e999df10ee49cc9457befe13201f68d5221ecc3c
kayryu <lukayryu@gmail.com>
2025-12-11 16:20:18 +0800

    feat: expand options

--- src/config.rs
+++ src/config.rs
@@ -5,20 +5,27 @@ use serde::{Deserialize, Serialize};
 pub struct RocksdbOptions {
     // TODO add more options for better usage
     pub create_if_missing: bool,
+    /// default false
     pub atomic_flush: bool,
+    /// default true
+    pub use_fsync: bool,
     // default 2
     pub log_file_num: Option<usize>,
     // default 20M
     pub log_file_size: Option<usize>,
+    // default 10M
+    pub max_total_wal_size: Option<u64>,
 }
 
 impl Default for RocksdbOptions {
     fn default() -> Self {
         RocksdbOptions {
             create_if_missing: true,
-            atomic_flush: true,
+            atomic_flush: false,
+            use_fsync: true,
             log_file_num: Some(2),
-            log_file_size: Some(20 * 1000 * 1000)
+            log_file_size: Some(20 * 1000 * 1000),
+            max_total_wal_size: Some(10 * 1 << 20)
         }
     }
 }
@@ -30,6 +37,8 @@ impl From<RocksdbOptions> for Options {
         opt.set_atomic_flush(roc_opt.atomic_flush);
         opt.set_keep_log_file_num(roc_opt.log_file_num.unwrap_or(2));
         opt.set_max_log_file_size(roc_opt.log_file_size.unwrap_or(20 * 1000 * 1000));
+        opt.set_max_total_wal_size(roc_opt.max_total_wal_size.unwrap_or(10 * 1 << 20));
+        opt.set_use_fsync(roc_opt.use_fsync);
         opt
     }
 }
